27b1d620fe4744bbf8d7fab0111804f2e795a2f7
jnanesw <nanimca000@gmail.com>
2025-10-28 22:29:02 +0530

    Resolving crash in Railway

--- src/main/java/com/ecommerce/project/config/WebConfig.java
+++ src/main/java/com/ecommerce/project/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.ecommerce.project.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,12 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${frontend.url}")
-    private String frontendUrl;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl, "http://localhost:5173/", "http://localhost:3000/", "https://cdg86r-5173.csb.app/")
+                .allowedOrigins("https://cdg86r-5173.csb.app/", "http://localhost:5173/", "http://localhost:3000/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
