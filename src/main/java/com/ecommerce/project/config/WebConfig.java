48d1702a65ca7d92cf398e19092d624d4d2a88ed
jnanesw <nanimca000@gmail.com>
2025-10-28 20:31:45 +0530

    Adding frontend url

---
+++ src/main/java/com/ecommerce/project/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.ecommerce.project.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontendUrl, "http://localhost:5173/", "http://localhost:3000/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
